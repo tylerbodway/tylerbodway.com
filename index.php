b5d68777279e4a98e297f6aad4d84e5ced247950
Tyler Bodway <tylerbodway@freshlifechurch>
2015-12-20 11:11:55 -0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Tyler Bodway | Web Design + Development</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css" />
+	<link href='http://fonts.googleapis.com/css?family=Duru+Sans' rel='stylesheet' type='text/css'>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-47120534-1', 'tylerbodway.com');
+  ga('send', 'pageview');
+
+</script>
+
+</head>
+
+<body>
+	<header>
+		<span class="first">Tyler</span>
+		<span class="last">Bodway</span>
+		<span class="web">Web</span>
+		<span class="design">[Design + Development]</span>
+	</header>
+	<div class="info">
+		<p>tyler@tylerbodway.com</p>
+		<p>406.270.6204</p>
+		<p class="sub">Please Contact for Inquiries | Thank You</p>
+	</div>
+</body>
+
+</html>
